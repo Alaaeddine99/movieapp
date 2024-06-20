052026c8313f9a8dbb4daabece38741722735e60
Alaaeddine99 <ala.maamer12@gmail.com>
2024-06-20 09:33:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import AddMovie from './Components/Addmovie/AddMovie'
+import MovieList from './Components/MovieList/MovieList'
+import Search from './Components/SearchMovie/Search'
+import {movies} from './data/MovieData'
+const App = () => {
+  // nbich ysour des modifications lil state 
+  // n7otha fi state
+  const [ListMovie,setListMovie]=useState(movies)
+        {/* partie ajout de film Function  */}
+const handelAddMovie=(newMovie)=>{
+setListMovie(
+  [...ListMovie,newMovie]
+)
+}
+
+const removeMovie=(idM)=>{
+  setListMovie(
+    ListMovie.filter((el)=> el.id !== idM)
+  )
+  }
+// func recherhce
+
+const [nameSearch,setNAmeSearch]=useState('')
+
+  return (
+    <div>
+      {/* partie recherche  */}
+      <Search  setNAmeSearch={setNAmeSearch} />
+
+
+      <MovieList  removeMovie={removeMovie}  ListMovie={ListMovie} ListMovie={ListMovie}  nameSearch={nameSearch}/>
+
+      <hr/>
+      {/* partie ajout de film  */}
+      <AddMovie  handelAddMovie={handelAddMovie}/>
+    </div>
+  )
+}
+
+export default App
